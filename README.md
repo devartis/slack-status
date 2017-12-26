a7287292a7f176456c5cc0f8fbcd2dab46773041
Fernando Aramendi <fernando@devartis.com>
2017-12-26 11:11:12 -0300

    working version

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # slack-status
+
+Create Token here
+
+* https://api.slack.com/custom-integrations/legacy-tokens
+
+
+Inspiration
+
+* https://medium.com/slack-developer-blog/how-to-set-a-slack-status-from-other-apps-ab4eef871339
+* https://github.com/kuy/slack-loc/blob/master/scripts/update.sh
+* https://askubuntu.com/questions/258580/how-to-run-a-script-depending-on-internet-connection
--- slack.sh
+++ slack.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-TOKEN=xoxp----
+TOKEN=xoxp---
 SSID=$(iwgetid -r)
 
 if [ $SSID == 'devartisUFO' ]
 then
 	STATUS='At%20devartis'
-	EMOJI=''
+	EMOJI='devartis'
 else
 	STATUS='Remote'
-	EMOJI=''
+	EMOJI='house_with_garden'
 fi
 
-echo https://slack.com/api/users.profile.set\?profile\=%7B%0D%0A%20%20%20%20%22$STATUS%22%2C%0D%0A%20%20%20%20%22status_emoji%22%3A%20%22%3A$EMOJI%3A%22%0D%0A%7D\&token\=$TOKEN
-#curl -X POST https://slack.com/api/users.profile.set\?profile\=%7B%0D%0A%20%20%20%20%22statis%22%2C%0D%0A%20%20%20%20%22status_emoji%22%3A%20%22%3Adevartis%3A%22%0D%0A%7D\&token\=$TOKEN
+curl -X POST https://slack.com/api/users.profile.set\?profile\=%7B%0D%0A%20%20%20%20%22status_text%22%3A%20%22$STATUS%22%2C%0D%0A%20%20%20%20%22status_emoji%22%3A%20%22%3A$EMOJI%3A%22%0D%0A%7D\&token\=$TOKEN
