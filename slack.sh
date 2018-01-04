2e8abe526d71922b110af243bb2ca6d2c7c973be
German Krauss <german@devartis.com>
2018-01-04 11:38:29 -0300

    Change working directory before execution to find token.config

--- slack.sh
+++ slack.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+	SOURCE="$(readlink "$SOURCE")"
+	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd $DIR
+
 TOKEN=$(<token.config)
 SSID=$(iwgetid -r)
 
@@ -11,7 +21,7 @@ then
 	STATUS='At%20Keepcon'
 	EMOJI='keepcon'
 else
-	STATUS='Working remotely'
+	STATUS='Working%20remotely'
 	EMOJI='house_with_garden'
 fi
 
