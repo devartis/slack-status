95111432c81c9884ad9ca8bdf5185a27585a6ad8
Alejandro Sobko <alee-sk8@hotmail.com>
2018-01-04 23:03:24 -0300

    Use a specific file for slack-token and the script know where is it

--- slack.sh
+++ slack.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-	SOURCE="$(readlink "$SOURCE")"
-	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-cd $DIR
-
-TOKEN=$(<token.config)
+#NON_ROOT_USERNAME=$SUDO_USER
+TOKEN=$(</home/$SUDO_USER/.slack-token.config)
 SSID=$(iwgetid -r)
 
 if [ $SSID == 'devartisUFO' ]
